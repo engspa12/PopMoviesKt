78145d9e7122818328d83aec8ac311fb1f945788
Daniel Bedoya <arturo.lpc12@gmail.com>
2022-08-02 15:05:48 -0500

    Update Internet connection check

--- app/src/main/java/com/example/dbm/popularmovieskt/util/Validator.kt
+++ app/src/main/java/com/example/dbm/popularmovieskt/util/Validator.kt
@@ -2,6 +2,9 @@ package com.example.dbm.popularmovieskt.util
 
 import android.content.Context
 import android.net.ConnectivityManager
+import android.net.ConnectivityManager.*
+import android.net.NetworkCapabilities.*
+import android.os.Build
 import javax.inject.Inject
 
 class Validator @Inject constructor(
@@ -9,12 +12,28 @@ class Validator @Inject constructor(
 ): IValidator {
 
     override fun isOnline(): Boolean {
-        //Verify if there is internet connection, if so then update the screen with the news articles
-        //Otherwise show the message there is no internet connection
         val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
 
-        val activeNetwork = cm.activeNetworkInfo;
-        return activeNetwork != null && activeNetwork.isConnectedOrConnecting;
-    }
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            val activeNetwork = cm.activeNetwork ?: return false
+            val capabilities = cm.getNetworkCapabilities(activeNetwork) ?: return false
+            return when {
+                capabilities.hasTransport(TRANSPORT_WIFI) -> true
+                capabilities.hasTransport(TRANSPORT_CELLULAR) -> true
+                capabilities.hasTransport(TRANSPORT_ETHERNET) -> true
+                else -> false
+            }
+        } else {
+            cm.activeNetworkInfo?.run {
+                return when(type) {
+                    TYPE_WIFI -> true
+                    TYPE_MOBILE -> true
+                    TYPE_ETHERNET -> true
+                    else -> true
+                }
+            }
+        }
 
+        return false
+    }
 }
