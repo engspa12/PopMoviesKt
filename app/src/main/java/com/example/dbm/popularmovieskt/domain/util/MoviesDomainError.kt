3f7fdba59b87feb728905199b9f793dda262545a
Daniel Bedoya <daniel.bedoya@externos.bci.cl>
2023-07-12 00:23:44 -0500

    Create error classes

---
+++ app/src/main/java/com/example/dbm/popularmovieskt/domain/util/MoviesDomainError.kt
@@ -0,0 +1,8 @@
+package com.example.dbm.popularmovieskt.domain.util
+
+enum class MoviesDomainError {
+    NO_INTERNET_CONNECTION,
+    NO_FAVORITE_MOVIES_FOUND,
+    UNKNOWN,
+    GENERIC
+}
---
+++ app/src/main/java/com/example/dbm/popularmovieskt/presentation/model/MoviesViewError.kt
@@ -0,0 +1,9 @@
+package com.example.dbm.popularmovieskt.presentation.model
+
+enum class MoviesViewError {
+    NO_INTERNET_CONNECTION,
+    NO_FAVORITE_MOVIES_FOUND,
+    UNKNOWN,
+    GENERIC,
+    NONE
+}
